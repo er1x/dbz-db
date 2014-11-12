8c3176ab2a1317533b52284cd4cc41c0ba975f16
er1x <erixcbr@gmail.com>
2014-11-12 20:05:58 +0100

    Vistas y rutas

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,19 @@
 
-angular.module('dbzDb', [
-  'dbzDb.services'
+var dbzDb = angular.module('dbzDb', [
+  'dbzDb.services',
+  'ngRoute'
 ]);
+
+
+dbzDb.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/list.html'
+    })
+    .when('/:id', {
+      templateUrl: 'partials/detail.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    }); 
+}]);
